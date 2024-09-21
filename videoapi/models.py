41473ed5103396a3458775a5e81a4535d439cd2b
Aman Gora <amangora@Amans-MacBook-Pro.local>
2024-09-21 19:38:43 +0530

    ft: Add video model with extended attributes for processing state

--- videoapi/models.py
+++ videoapi/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    video_file = models.FileField(upload_to='videos/')
+    is_merged = models.BooleanField(default=False)
+    is_trimmed = models.BooleanField(default=False)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
