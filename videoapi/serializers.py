d3ee78d3255712de84714d9f8dd42b8660520882
Aman Gora <amangora1999@gmail.com>
2024-09-22 23:10:10 +0530

    Fix: video upload without title

--- videoapi/serializers.py
+++ videoapi/serializers.py
@@ -2,17 +2,18 @@
 from rest_framework import serializers
 from .models import Video
 from moviepy.editor import VideoFileClip
+import os
 
 class VideoSerializer(serializers.ModelSerializer):
     file_size = serializers.SerializerMethodField()
+    title = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = Video
-        fields = ['id', 'title', 'is_merged','video_file', 'uploaded_at', 'file_size','is_trimmed']
+        fields = ['id', 'title', 'is_merged', 'video_file', 'uploaded_at', 'file_size', 'is_trimmed']
 
     def get_file_size(self, obj):
         return obj.video_file.size
-
 
     def validate_video_file(self, value):
         # Define the minimum and maximum file size in bytes
@@ -38,3 +39,11 @@ class VideoSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(f"Video must be between {min_duration} and {max_duration} seconds long")
 
         return value
+
+    def validate(self, attrs):
+        # If title is missing or blank, set it to the default value based on the file name
+        if not attrs.get('title'):
+            video_file = attrs.get('video_file')
+            if video_file:
+                attrs['title'] = os.path.splitext(video_file.name)[0]
+        return attrs
