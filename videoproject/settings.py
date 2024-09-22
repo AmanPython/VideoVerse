144aa39ddfd969175cdae082d9f3b4163181c7ac
Aman Gora <amangora1999@gmail.com>
2024-09-22 23:47:02 +0530

    Ft: Celery Beat Initialization

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'videoapi',
     'corsheaders',
     'accounts',
-    'drf_yasg'
+    'drf_yasg',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
@@ -154,4 +155,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
