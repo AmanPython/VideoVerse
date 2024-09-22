795cfea26e185ba1652c3d1d305b5ac865b80c3f
Aman Gora <amangora1999@gmail.com>
2024-09-22 23:05:04 +0530

    ft: email and username validation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,24 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
 
     def create(self, validated_data):
         user = User(username=validated_data['username'], email=validated_data['email'])
